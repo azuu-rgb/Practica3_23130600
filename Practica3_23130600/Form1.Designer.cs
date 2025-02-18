745e8e44c87396287042a73bf131bd555e9b7bd6
azuu-rgb <azurodriguezflores27@gmail.com>
2025-02-17 21:40:06 -0600

    17 DE FEBRERO se realizo la practica

--- Practica3_23130600/Form1.Designer.cs
+++ Practica3_23130600/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Practica3_23130600
 {
-    partial class Form1
+    partial class formData
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            openFileDialog1 = new OpenFileDialog();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            menuStrip1 = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            abrirToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "Archivo CSV|*.csv";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(4, 36);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(805, 287);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(84, 362);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 1;
+            button1.Text = "Mostrar ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(809, 33);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { abrirToolStripMenuItem });
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(88, 29);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            abrirToolStripMenuItem.Size = new Size(153, 34);
+            abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // formData
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(809, 450);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "formData";
+            Text = "Menu";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
     }
 }
--- Practica3_23130600/Form1.cs
+++ Practica3_23130600/Form1.cs
@@ -1,10 +1,74 @@
+using System.Windows.Forms;
+
 namespace Practica3_23130600
 {
-    public partial class Form1 : Form
+    public partial class formData : Form
     {
-        public Form1()
+        public formData()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = openFileDialog1.ShowDialog();
+
+            if (resultado == DialogResult.OK)
+            {
+                string filePatch = openFileDialog1.FileName;
+                string texto = File.ReadAllText(filePatch);
+
+                char[] delimitadores = { ',', '\n' };
+                string[] lineas = texto.Split("\n");
+                string[] palabras = texto.Split(delimitadores);
+                string[][] palabrasMatriz = new string[lineas.Length][];
+
+
+                int cont = 0;
+
+                //contamos las comas
+                foreach (char c in lineas[0])
+                {
+                    if (c == ',')
+                    {
+                        cont++;
+                    }
+                }
+
+                //se crean las columnas 
+
+                for (int i = 0; i < cont + 1; i++)
+                {
+                    dataGridView1.Columns.Add(palabras[i], palabras[i]);
+                };
+
+                //guarda todas las palabras en una mtriz para pasarla al showGrid  
+               
+
+                for (int i = 1; i < lineas.Length; i++)
+                {
+                    
+                    palabrasMatriz[i] = lineas[i].Split(delimitadores);
+                }
+
+                for (int i = 1; i < palabrasMatriz.Length - 1; i++)
+                {
+
+                    string[] fila = palabrasMatriz[i];
+
+
+                    dataGridView1.Rows.Add(fila);
+                }
+
+
+            }
+        }
+
+    
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
         }
     }
 }
