a7a8e06110a1a3b4277d01f9cc95aec669c06cbb
azuu-rgb <azurodriguezflores27@gmail.com>
2025-02-18 09:56:38 -0600

    Martes 18 de febrero hice los cambios que se me ocurrieron en la noche

--- Practica3_23130600/Form1.Designer.cs
+++ Practica3_23130600/Form1.Designer.cs
@@ -31,60 +31,33 @@
             openFileDialog1 = new OpenFileDialog();
             dataGridView1 = new DataGridView();
             button1 = new Button();
-            menuStrip1 = new MenuStrip();
-            archivoToolStripMenuItem = new ToolStripMenuItem();
-            abrirToolStripMenuItem = new ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // openFileDialog1
             // 
             openFileDialog1.FileName = "openFileDialog1";
             openFileDialog1.Filter = "Archivo CSV|*.csv";
-            openFileDialog1.FileOk += openFileDialog1_FileOk;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(4, 36);
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(805, 287);
+            dataGridView1.Size = new Size(809, 450);
             dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(84, 362);
+            button1.Location = new Point(12, 404);
             button1.Name = "button1";
             button1.Size = new Size(112, 34);
             button1.TabIndex = 1;
             button1.Text = "Mostrar ";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
-            // 
-            // menuStrip1
-            // 
-            menuStrip1.ImageScalingSize = new Size(24, 24);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem });
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(809, 33);
-            menuStrip1.TabIndex = 2;
-            menuStrip1.Text = "menuStrip1";
-            // 
-            // archivoToolStripMenuItem
-            // 
-            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { abrirToolStripMenuItem });
-            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            archivoToolStripMenuItem.Size = new Size(88, 29);
-            archivoToolStripMenuItem.Text = "Archivo";
-            // 
-            // abrirToolStripMenuItem
-            // 
-            abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
-            abrirToolStripMenuItem.Size = new Size(153, 34);
-            abrirToolStripMenuItem.Text = "Abrir";
             // 
             // formData
             // 
@@ -93,15 +66,10 @@
             ClientSize = new Size(809, 450);
             Controls.Add(button1);
             Controls.Add(dataGridView1);
-            Controls.Add(menuStrip1);
-            MainMenuStrip = menuStrip1;
             Name = "formData";
             Text = "Menu";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -109,8 +77,5 @@
         private OpenFileDialog openFileDialog1;
         private DataGridView dataGridView1;
         private Button button1;
-        private MenuStrip menuStrip1;
-        private ToolStripMenuItem archivoToolStripMenuItem;
-        private ToolStripMenuItem abrirToolStripMenuItem;
     }
 }
--- Practica3_23130600/Form1.cs
+++ Practica3_23130600/Form1.cs
@@ -4,12 +4,12 @@ namespace Practica3_23130600
 {
     public partial class formData : Form
     {
+        bool x = false;
         public formData()
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        public void archivoCSV ()
         {
             DialogResult resultado = openFileDialog1.ShowDialog();
 
@@ -19,8 +19,8 @@ namespace Practica3_23130600
                 string texto = File.ReadAllText(filePatch);
 
                 char[] delimitadores = { ',', '\n' };
+
                 string[] lineas = texto.Split("\n");
-                string[] palabras = texto.Split(delimitadores);
                 string[][] palabrasMatriz = new string[lineas.Length][];
 
 
@@ -34,41 +34,45 @@ namespace Practica3_23130600
                         cont++;
                     }
                 }
-
-                //se crean las columnas 
-
-                for (int i = 0; i < cont + 1; i++)
+                //guarda todas las palabras en una mtriz para pasarla al showGrid 
+                for (int i = 0; i < lineas.Length; i++)
                 {
-                    dataGridView1.Columns.Add(palabras[i], palabras[i]);
-                };
 
-                //guarda todas las palabras en una mtriz para pasarla al showGrid  
-               
-
-                for (int i = 1; i < lineas.Length; i++)
-                {
-                    
                     palabrasMatriz[i] = lineas[i].Split(delimitadores);
                 }
 
+                //se crean las columnas
+                for (int i = 0; i < cont + 1; i++)
+                {
+                    dataGridView1.Columns.Add(palabrasMatriz[0][i], palabrasMatriz[0][i]);
+                };
+
                 for (int i = 1; i < palabrasMatriz.Length - 1; i++)
                 {
-
                     string[] fila = palabrasMatriz[i];
-
-
                     dataGridView1.Rows.Add(fila);
                 }
+                x = true;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
 
+            if (x = false)
+            {
+                archivoCSV();
+
+               
+            }
+            else
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                archivoCSV();
 
             }
         }
-
-    
-
-        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-
-        }
     }
 }
+    
+
